7ea3fa175f7e625040b213bb07f1375175dd6ad2
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-10-13 20:54:03 +0700

    feat: add function to retrieve items from backend

--- src/pages/components/DatabaseItem.tsx
+++ src/pages/components/DatabaseItem.tsx
@@ -1,7 +1,27 @@
 "use client"
 
+export async function getServerSideProps() {
+  try {
+    // API endpoint not yet implemented
+    const response = await fetch("http://localhost");
+    const data = await response.json();
+    return {
+      props: {
+        items: data,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching file, ', error);
+    return {
+      props: {
+        items: [],
+      },
+    };
+  }
+}
+
 export default function DatabaseItem() {
-    return (
-        <div>This element from DatabaseItem's component!</div>
-    )
+  return (
+    <div>This element from DatabaseItem's component!</div>
+  )
 }
