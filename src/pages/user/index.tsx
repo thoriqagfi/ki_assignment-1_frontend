9ed56335dc855f465e52f6b495fa0ddd1f990b5a
urdhanaka <urdhanaka.aptanagi@gmail.com>
2023-10-13 20:55:32 +0700

    feat: implement DatabaseItems

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,13 +1,19 @@
+import { DatabaseItem } from "../components"
+
 export default function User() {
   return (
     <main className="flex w-screen h-screen justify-center">
       <div className="mx-auto my-auto w-fit h-fit flex flex-col border-2 p-4 border-slate-950 gap-4 rounded-lg shadow-2xl">
         <div className="flex justify-between">
           <h1 className="font-bold text-2xl">Hello, user!</h1>
-          <p className="my-auto">This should be on the right</p>
         </div>
+        <ul>
+          <li><DatabaseItem></DatabaseItem></li>
+          <li><DatabaseItem></DatabaseItem></li>
+          <li><DatabaseItem></DatabaseItem></li>
+          <li><DatabaseItem></DatabaseItem></li>
+        </ul>
       </div>
     </main >
   )
 }
-
